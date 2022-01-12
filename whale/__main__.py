6c241029874e284f690d9b12a882a777740767fd
anime fanatic <beol0127@gmail.com>
2022-01-12 13:12:38 +0000

    chore: update buy cmd and update embed

--- whale/__main__.py
+++ whale/__main__.py
@@ -4,15 +4,26 @@ import os
 import random
 import json
 from datetime import datetime
-import threading
 from emoji_game import EmojiGame
 from shop import Shop
 from memory_game import MemoryGame
 from replit import db
 from datetime import datetime
 
+def has(item, id):
+      hasItem = False
+      for key in db[str(id)]["items"].keys():
+        if key == item:
+          hasItem = True
+      return hasItem
+
 intents = discord.Intents.default()
 # intents.members = True
+
+db["shop_items"] = {
+  "apple": 200,
+  "sunsettia": 200
+}
 
 client = discord.Client(intents=intents)
 
@@ -55,7 +66,8 @@ async def on_message(msg):
 `{PREFIX}shop`: View shop to get items.
 `{PREFIX}buy <item>`: Buy item from the shop.
 `{PREFIX}stats`: View health and damage stats.
-`{PREFIX}inv`: View current items and foods.""")
+`{PREFIX}inv`: View current items and foods.
+`{PREFIX}upgrade`: Spend Mora to upgrade stats (health or damage).""")
     await msg.channel.send(embed = builder)
     await msg.channel.send("> Tip: You can shorten the function name for easier access!")
 
@@ -85,15 +97,9 @@ async def on_message(msg):
     all_count = random.randint(1, 7)
     items2 = ["apple", "wood", "egg", "sunsettia"]
     item2 = random.choice(items2)
-    def has(item):
-      hasItem = False
-      for key in db[str(msg.author.id)]["items"].keys():
-        if key == item:
-          hasItem = True
-      return hasItem
     # print(item2)
     if item2 == "apple":
-      if has("apple"):
+      if has("apple", msg.author.id):
         db[str(msg.author.id)]["items"]["apple"]["count"] += all_count
       else:
         db[str(msg.author.id)]["items"]["apple"] = {
@@ -101,7 +107,7 @@ async def on_message(msg):
           "name": "Apples"
         } # code go brrrrr
     if item2 == "egg":
-      if has("egg"):
+      if has("egg", msg.author.id):
         db[str(msg.author.id)]["items"]["egg"]["count"] += all_count
       else:
         db[str(msg.author.id)]["items"]["egg"] = {
@@ -109,7 +115,7 @@ async def on_message(msg):
           "name": "Eggs"
         }
     if item2 == "sunsettia":
-      if has("sunsettia"):
+      if has("sunsettia", msg.author.id):
         db[str(msg.author.id)]["items"]["sunsettia"]["count"] += all_count
       else:
         db[str(msg.author.id)]["items"]["sunsettia"] = {
@@ -117,7 +123,7 @@ async def on_message(msg):
           "name": "Sunsettias"
         } 
     if item2 == "wood":
-      if has("wood"):
+      if has("wood", msg.author.id):
         db[str(msg.author.id)]["items"]["wood"]["count"] += all_count
       else:
         db[str(msg.author.id)]["items"]["wood"] = {
@@ -138,7 +144,36 @@ async def on_message(msg):
     await shop.printShop()
 
   if MSG.startswith(f"{PREFIX}buy"):
-    pass
+    tokens = MSG.split(" ")
+    if len(tokens) != 1:
+      multip = 1
+      try:
+        multip = int(tokens[2])
+      except:
+        pass # put error here later
+      item_to_buy = tokens[1]
+      shop_items = db["shop_items"]
+      try:
+        shop_items[item_to_buy]
+      except:
+        await msg.channel.send(f"{msg.author.mention}, {item_to_buy} is not an item in the shop. Please use `k!shop` to see a list of items")
+      cost = shop_items[item_to_buy] * multip
+      user_mora = db[str(msg.author.id)]["mora"]
+      if user_mora >= cost:
+        db[str(msg.author.id)]["mora"] -= cost
+        if has(item_to_buy, msg.author.id):
+          db[str(msg.author.id)]["items"][item_to_buy]["count"] += 1 * multip
+        else:
+          db[str(msg.author.id)]["items"][item_to_buy] = {
+            "name": item_to_buy,
+            "count": 1 * multip
+          }
+        await msg.channel.send("You bought " + str(multip) + " " + item_to_buy + " for " + str(cost) + " mora!")
+      else:
+        await msg.channel.send("You don't have enough mora!")
+    else:
+      await msg.channel.send("Please specify an item to buy. Use `k!shop` to see a full list of items")
+
     
   if MSG.startswith(f"{PREFIX}bal") or MSG.startswith(f"{PREFIX}balance") or MSG.startswith(f"{PREFIX}mora"):
     checkIfUserExists(msg.author.id)
@@ -157,7 +192,30 @@ async def on_message(msg):
 
   if MSG.startswith(f"{PREFIX}stats") or MSG.startswith(f"{PREFIX}st"):
     checkIfUserExists(msg.author.id)
-    await msg.channel.send("Health: " + str(db[str(msg.author.id)]["health"]) + "\nDamage: " + str(db[str(msg.author.id)]["damage"]))
+    builder = discord.Embed(title = "Stats", name = "Stats", description = ":heart: Health: " + str(db[str(msg.author.id)]["health"]) + "\n:crossed_swords: Damage: " + str(db[str(msg.author.id)]["damage"]))
+    await msg.channel.send(embed = builder)
+
+  if MSG.startswith(f"{PREFIX}upgrade") or MSG.startswith(f"{PREFIX}u"):
+    a = random.choice([True, False])
+
+    if a:
+      healthp = random.randint(10, 50)
+      db[str(msg.author.id)]["health"] += healthp
+      if db[str(msg.author.id)]["mora"] >= 1000:
+        db[str(msg.author.id)]["mora"] -= 1000
+        builder = discord.Embed(title = "Upgrade", name = "Upgrade", description = ":heart: Health has been increased by " + str(healthp) + "!")
+        await msg.channel.send(embed = builder)
+      else:
+        await msg.channel.send("You do not have enough Mora!")
+    else:
+      dmgp = random.randint(5, 20)
+      db[str(msg.author.id)]["damage"] += dmgp
+      if db[str(msg.author.id)]["mora"] >= 1000:
+        db[str(msg.author.id)]["mora"] -= 1000
+        builder = discord.Embed(title = "Upgrade", name = "Upgrade", description = ":crossed_swords: Damage has been increased by " + str(dmgp) + "!")
+        await msg.channel.send(embed = builder)
+      else:
+        await msg.channel.send("You do not have enough Mora!")
 
 #   if MSG.startswith(f"{PREFIX}battle"):
   #   boss = random.choice([""])
